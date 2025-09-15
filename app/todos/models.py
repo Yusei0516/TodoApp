63e45c6b3e363204dc8b65594df9e7840ec27ebe
Yusei0516 <yusei130516@gmail.com>
2025-09-15 15:51:34 +0900

    modelsの作成

---
+++ app/todos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos"
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
---
+++ app/todos/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.shortcuts import render
+
+
+# Create your views here.
